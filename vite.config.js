af5e8de7fde213965f690de28b0e111b1774bb74
Abdoul977 <nassermaiga7@gmail.com>
2025-01-26 12:15:18 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,21 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  return {
-    server: {
-      host: true,
-      port: 5173
-    },
-    plugins: [react()],
-    build: {
-      outDir: 'dist',
-      sourcemap: true,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            vendor: ['react', 'react-dom', 'react-router-dom']
-          }
+export default defineConfig({
+  server: {
+    host: true,
+    port: 5173
+  },
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom']
         }
       }
-    },
-    define: {
-      'process.env.VITE_APP_URL': JSON.stringify(env.VITE_APP_URL),
-      'process.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL), 
-      'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY)
     }
   }
 })
